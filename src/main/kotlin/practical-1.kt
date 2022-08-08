9338af520718e02cc88b65b4ce9e6a40b0b581bc
UrvaSuthar <urvasuthar2@gmail.com>
2022-08-08 10:04:37 +0530

    Practial - 1

--- src/main/kotlin/practical-1.kt
+++ src/main/kotlin/practical-1.kt
@@ -1,3 +1,22 @@
 fun main(){
-    print("Hello World")
+//    print("Hello World")
+    val integer : Int = 8
+    println("Integer : $integer")
+    val float = 1.8F
+    println("Float : $float")
+    val character : Char = 'a'
+    println("Character : $character")
+    val string : String = "Hello"
+    println("String : $string")
+    val bool : Boolean = true
+    println("Boolean : $bool")
+    val double : Double = 89.3425
+    println("Double : $double")
+    val long : Long = 20012021062
+    println("Long  : $long")
+    val short : Short = -2
+    println("Short : $short")
+    val byte : Byte = 127
+    println("Byte : $byte")
+
 }

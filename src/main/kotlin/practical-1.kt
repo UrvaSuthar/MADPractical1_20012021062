80d1be18e5c279f3ac04e69071fe69b12d6d275f
UrvaSuthar <urvasuthar2@gmail.com>
2022-08-08 10:52:43 +0530

    Practial - 1

--- src/main/kotlin/practical-1.kt
+++ src/main/kotlin/practical-1.kt
@@ -1,22 +1,39 @@
-fun main(){
+fun main() {
 //    print("Hello World")
-    val integer : Int = 8
+    val integer: Int = 8
     println("Integer : $integer")
     val float = 1.8F
     println("Float : $float")
-    val character : Char = 'a'
+    val character: Char = 'a'
     println("Character : $character")
-    val string : String = "Hello"
+    val string: String = "Hello"
     println("String : $string")
-    val bool : Boolean = true
+    val bool: Boolean = true
     println("Boolean : $bool")
-    val double : Double = 89.3425
+    val double: Double = 89.3425
     println("Double : $double")
-    val long : Long = 20012021062
+    val long: Long = 20012021062
     println("Long  : $long")
-    val short : Short = -2
+    val short: Short = -2
     println("Short : $short")
-    val byte : Byte = 127
+    val byte: Byte = 127
     println("Byte : $byte")
+    p2()
+}
+fun p2(){
+    println("========================Type Conversion========================")
 
+    val x = 5
+    println("Integer : $x")
+    x.toDouble()
+    println("Integer to Double : $x ")
+
+    val string = "10"
+    println("String : $string")
+    string.toInt()
+    println("String To Integer : $string")
+
+    val string2 = "10.5"
+    string2.toDouble()
+    println("String To Double : $string2")
 }
